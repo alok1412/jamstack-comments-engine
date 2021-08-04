58ccdcdf6522ccadeafffb0b87bcd7f847bd9f67
alok1412 <81139845+alok1412@users.noreply.github.com>
2021-08-04 14:25:35 +0530

    Update comment-action.js

--- src/functions/comment-action.js
+++ src/functions/comment-action.js
@@ -7,7 +7,7 @@ const {
 } = process.env;
 
 // hardcoding this for a moment... TODO: replace request with somethign that follows redirects
-const URL = "https://elastic-blackwell-834779.netlify.app/";
+const URL = "https://elastic-blackwell-834779.netlify.app";
 
 /*
   delete this submission via the api
