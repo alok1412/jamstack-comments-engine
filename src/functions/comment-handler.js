101ebe5f79a68aaf6481264aa715a23deb797c7b
alok1412 <81139845+alok1412@users.noreply.github.com>
2021-08-04 14:49:29 +0530

    Update comment-handler.js

--- src/functions/comment-handler.js
+++ src/functions/comment-handler.js
@@ -5,7 +5,7 @@ var request = require("request");
 // populate environment variables locally.
 require('dotenv').config()
 
-const URL = "https://elastic-blackwell-834779.netlify.app";
+const URL = "https://elastic-blackwell-834779.netlify.app/";
 
 /*
   Our serverless function handler
